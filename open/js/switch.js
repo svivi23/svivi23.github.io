7243c8e8fdb22994e5492db4daddb87acfdbec7e
svivi23 <125348477+svivi23@users.noreply.github.com>
2023-08-15 11:34:23 +0800

    Add files via upload

---
+++ open/js/switch.js
@@ -0,0 +1,104 @@
+function lotteryarray() {
+    var check = document.getElementById("switchID1");
+    var op = document.getElementById("id01");
+    var sb = document.getElementById("id02");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+
+    var check = document.getElementById("switchID2");
+    var op = document.getElementById("id01-2");
+    var sb = document.getElementById("id02-2");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+
+    var check = document.getElementById("switchID3");
+    var op = document.getElementById("id01-3");
+    var sb = document.getElementById("id02-3");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+
+    var check = document.getElementById("switchID4");
+    var op = document.getElementById("id01-4");
+    var sb = document.getElementById("id02-4");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+
+    var check = document.getElementById("switchID5");
+    var op = document.getElementById("id01-5");
+    var sb = document.getElementById("id02-5");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+
+    var check = document.getElementById("switchID6");
+    var op = document.getElementById("id01-6");
+    var sb = document.getElementById("id02-6");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+    var check = document.getElementById("switchID7");
+    var op = document.getElementById("id01-7");
+    var sb = document.getElementById("id02-7");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+    var check = document.getElementById("switchID8");
+    var op = document.getElementById("id01-8");
+    var sb = document.getElementById("id02-8");
+    if (check.checked == true) {
+        sb.style.display = 'block';
+        op.style.display = 'none';
+
+
+    } else {
+        op.style.display = 'block';
+        sb.style.display = 'none';
+    }
+
+}
